ed5629b80cf71f22435c6888f62a6857baf79869
amandarice <amandarice@Amandas-MacBook-Air.local>
2016-12-16 10:32:05 -0600

    Initial commit of CodeComments

---
+++ README.md
@@ -0,0 +1,38 @@
+# CodeComments
+
+CodeComments is an Alexa skill that provides provides how to comment in a user-supplied programming language.
+
+Sample Interaction:
+
+* Your Question: "Alexa, ask Code Comments how do I comment in Python?"
+* Alexa's Response: "Just use the hash character at the beginning of the comment for each line of code."
+
+This skill is primarily intended for programmers needing a quick reminder on how to comment in a certain language. There are currently 16 programming language responses available.
+
+### Project Format
+
+The speech_assets folder contains the files which comprise the Voice User Interface:
+* intent schema in intent_schema.json
+* sample utterances in utterances.txt
+* custom slot values in LIST_OF_LANGUAGES.txt, which contains a list of programming languages supported by this skill
+
+The ask folder is based on the ask-alexa-pykit and is used to help develop an Alexa skill in Python.
+
+The lambda function in lambda_function.py is used to run the Alexa skill using AWS Lambda.
+
+### To Upload to Lambda
+
+Right click all of the following, then compress:
+* ask folder
+* lambda_function.py
+* comments.txt
+
+Upload the compressed zip to Lambda.
+
+### Testing
+
+Text/JSON calls and responses - https://developer.amazon.com/edw/home.html
+
+Vocal calls and responses - https://echosim.io/
+
+**Written by Amanda Rice**
---
+++ lambda_function.py
@@ -0,0 +1,46 @@
+from ask import alexa
+import urllib2
+
+def lambda_handler(request_obj, context={}):
+    return alexa.route_request(request_obj)
+
+@alexa.default_handler()
+def default_handler(request):
+    return launch_request_handler(request)
+
+@alexa.request_handler("LaunchRequest")
+def launch_request_handler(request):
+    return alexa.create_response(message="Welcome to Code Comments! If you give me the name of a programming language, I will respond with instructions for making a comment in that language! In what language would you like to comment?",
+                                 reprompt_message="Learning how to comment can be fun! What language would you like to comment out?")
+
+@alexa.request_handler(request_type="SessionEndedRequest")
+def session_ended_request_handler(request):
+    return alexa.create_response(message="Goodbye!", end_session=True)
+
+@alexa.intent_handler("GetProgrammingLanguage")
+def get_animal_sound_handler(request):
+
+    language = request.get_slot_value("ProgrammingLanguage")
+    comments = {}
+    with open("comments.txt") as f:
+        for line in f:
+            newline = line.strip()
+            key = newline[:newline.find(' ')]
+            val = newline[newline.find(' ')+1:]
+            comments[key]=val
+    if language in comments:
+        return alexa.create_response(message=comments[language] + ".", end_session=True)
+    else:
+        return alexa.create_response(message="We don't know how to comment in " + language + ". Please try again.", end_session=False)
+
+@alexa.intent_handler("AMAZON.HelpIntent")
+def help_intent_handler(request):
+    return alexa.create_response(message="I will give you instructions on how to comment in a language you provide me.", end_session=False)
+
+@alexa.intent_handler("AMAZON.StopIntent")
+def stop_intent_handler(request):
+    return alexa.create_response(message="Goodbye!", end_session=True)
+
+@alexa.intent_handler("AMAZON.CancelIntent")
+def cancel_intent_handler(request):
+    return alexa.create_response(message="Goodbye!", end_session=True)
